e55599cae2d1ca13c67776b7ed9ba9f983ae6b22
YitavGil <yitavga@yahoo.com>
2022-05-07 08:08:55 +0300

    connect to server

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,31 +1,40 @@
 import "./home.scss";
-
 import { useState, useEffect } from "react";
-import Navbar from '../../components/navbar/Navbar';
+import axios from "axios";
+
+import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
 
-const Home = ({type}) => {
+const Home = ({ type }) => {
   const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
 
   useEffect(() => {
     const getRandomList = async () => {
       try {
-        
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${genre ? "&genre=" + genre : ""}`
+        );
+        console.log(res);
+        // setLists(res.data);
       } catch (error) {
         console.log(error);
       }
-    }
-  },[])
+    };
+    getRandomList();
+  }, [type, genre]);
 
-  return <div className="home">
-    <Navbar />
-    <Featured type={type} />
-    <List />
-    <List />
-    <List />
-    <List />
-  </div>;
+  return (
+    <div className="home">
+      <Navbar />
+      <Featured type={type} />
+      <List />
+      <List />
+      <List />
+      <List />
+    </div>
+  );
 };
 
 export default Home;
